fcfd0f19f7d0e4d364117761f378dc29dc1a8ece
Sugizou0215 <sugizou0215@gmail.com>
2021-08-29 10:24:58 +0000

    [Update devise]8-10

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'homes/top'
+  get 'homes/about'
+  root to: 'homes#top'
+
+  resources :books, except: [:new]
+
+  resources :users, except: [:new, :create]
 end
