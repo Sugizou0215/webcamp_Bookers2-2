e72d8cad59ef8914abac3e500b50fd3bfe9fbcce
Sugizou0215 <sugizou0215@gmail.com>
2021-09-01 10:22:40 +0000

    [Finish]2-4

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,10 @@ Rails.application.routes.draw do
     resources :post_comments, only: [:create, :destroy]
   end
 
-  resources :users, except: [:new, :create, :destroy]
+  resources :users, except: [:new, :create, :destroy] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
+  
 end
