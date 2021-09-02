9e1fa223129253dcf8c683b185276b2771123155
Sugizou0215 <sugizou0215@gmail.com>
2021-09-02 10:28:24 +0000

    [Finish]2-6

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,18 +1,16 @@
 class PostCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = current_user.post_comments.new(post_comment_params)
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    redirect_to request.referer
   end
 
   def destroy
     @book = Book.find(params[:book_id])
   	post_comment = @book.post_comments.find(params[:id])
 		post_comment.destroy
-		redirect_to request.referer
   end
 
   def post_comment_params
